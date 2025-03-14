b6298714caaceb27cb5067a94f5bfa9d5c1f8eef
Lance Adrian Acal <lncadrnn@users.noreply.github.com>
2025-03-14 16:17:35 +0800

    next step is to add database

--- src/main/java/com/example/chandriaslarobe/LoginController.java
+++ src/main/java/com/example/chandriaslarobe/LoginController.java
@@ -2,13 +2,33 @@ package com.example.chandriaslarobe;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
+
 
 public class LoginController {
 
     @FXML
     private Button cancelButton;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField enterPasswordField;
+
+
+    public void loginButtonOnAction(ActionEvent event) {
+
+        if (usernameTextField.getText().isBlank() == false && enterPasswordField.getText().isBlank() == false) {
+            validateLogin();
+        } else {
+            loginMessageLabel.setText("Please enter username and password.");
+        }
+    }
 
     public void cancelButtonOnAction(ActionEvent event) {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
